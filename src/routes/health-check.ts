e8738b36d1e8f242413f583b393a9769bfa59dfd
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-07-21 22:47:59 -0300

    feat: adiciona suporte para AWS App Runner no workflow e registra rota de verificação de saúde no servidor

---
+++ src/routes/health-check.ts
@@ -0,0 +1,7 @@
+import type { FastifyInstance } from "fastify";
+
+export async function healthCheckRoute(app: FastifyInstance) {
+  app.get('/health-check', async (_, reply) => {
+    return reply.status(200).send({ message: 'OK' })
+  })
+}
