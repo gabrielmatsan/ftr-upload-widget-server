4973af62a92fc40dd95df31d5be2bc9f60f8624a
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-07-27 08:58:45 -0300

    feat: atualiza o workflow para usar Amazon ECS, substituindo a implementação do AWS App Runner e ajustando a rota de verificação de saúde para refletir a nova configuração

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health-check', async (_, reply) => {
-    return reply.status(200).send({ message: 'OK' })
+    return reply.status(200).send({ message: 'OK no ECS' })
   })
 }
